976977b472066b6cb1d8d976c5d04981a95c309b
Ovsyannikov_M <maksim.ov@mail.ru>
2022-08-11 00:37:41 +0300

    add city into map

--- app/src/main/java/com/example/weatherapp2/ui/maps/MapsFragment.kt
+++ app/src/main/java/com/example/weatherapp2/ui/maps/MapsFragment.kt
@@ -1,19 +1,36 @@
 package com.example.weatherapp2.ui.maps
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.example.weatherapp2.R
 import com.example.weatherapp2.databinding.FragmentMapBinding
+import com.example.weatherapp2.model.common.CityFullInfo
+import com.example.weatherapp2.model.db.DataBase
+import com.example.weatherapp2.model.repository.LocalRepoImpl
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import java.lang.Math.round
+import kotlin.math.roundToInt
 
 class MapsFragment : Fragment(), OnMapReadyCallback {
 
     private lateinit var mapBinding: FragmentMapBinding
+    private val mapsViewModel by viewModels<MapsViewModel> {
+        MapsViewModelFactory(
+            LocalRepoImpl(
+                DataBase.getDataBase(this.requireContext())!!
+                    .localDao()
+            )
+        )
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,11 +38,32 @@ class MapsFragment : Fragment(), OnMapReadyCallback {
         savedInstanceState: Bundle?
     ): View {
         mapBinding = FragmentMapBinding.inflate(inflater, container, false)
-        val q = childFragmentManager.findFragmentById(R.id.google_map) as SupportMapFragment
-        q.getMapAsync(this)
+        val supportMapFragment = childFragmentManager.findFragmentById(R.id.google_map) as SupportMapFragment
+        supportMapFragment.getMapAsync(this)
         return mapBinding.root
     }
 
-    override fun onMapReady(p0: GoogleMap) {
+    override fun onMapReady(googleMap: GoogleMap) {
+        Log.d("Map", "onMapReady")
+        googleMap.uiSettings.isZoomControlsEnabled = true
+        mapsViewModel.getCitiesFullInfo().observe(viewLifecycleOwner){ citiesFullInfo ->
+            val cityMarkers = createCityMarkers(citiesFullInfo)
+            cityMarkers.forEach { googleMap.addMarker(it) }
+        }
+    }
+
+    private fun createCityMarkers(citiesFullInfo: List<CityFullInfo>): List<MarkerOptions> {
+        val markers = mutableListOf<MarkerOptions>()
+        for (cityInfo in citiesFullInfo) {
+            val marker = MarkerOptions()
+            if(cityInfo.comment!="") {
+                marker.snippet(cityInfo.comment)
+            }
+            marker.position(LatLng(cityInfo.lat, cityInfo.lon))
+            val temp = requireContext().getString(R.string.celsius, cityInfo.current!!.temp.roundToInt())
+            marker.title("${cityInfo.name}, ${cityInfo.country} $temp")
+            markers.add(marker)
+        }
+        return markers
     }
 }
--- app/src/main/java/com/example/weatherapp2/ui/maps/MapsViewModel.kt
+++ app/src/main/java/com/example/weatherapp2/ui/maps/MapsViewModel.kt
@@ -3,11 +3,28 @@ package com.example.weatherapp2.ui.maps
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.weatherapp2.model.common.CityFullInfo
+import com.example.weatherapp2.model.repository.LocalRepo
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
 
-class MapsViewModel : ViewModel() {
+class MapsViewModel(private val localRepo: LocalRepo) : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is Maps Fragment"
+   private val citiesFullInfo: MutableLiveData<List<CityFullInfo>> by lazy{
+       MutableLiveData<List<CityFullInfo>>().also {
+            loadCitiesFullInfoFromRepo()
+       }
+   }
+
+    fun getCitiesFullInfo(): LiveData<List<CityFullInfo>>{
+        return citiesFullInfo
     }
-    val text: LiveData<String> = _text
+
+    private fun loadCitiesFullInfoFromRepo(){
+        CoroutineScope(SupervisorJob() + Dispatchers.IO).launch {
+            citiesFullInfo.postValue(localRepo.getAllCityFullInfo())
+        }
+    }
 }
