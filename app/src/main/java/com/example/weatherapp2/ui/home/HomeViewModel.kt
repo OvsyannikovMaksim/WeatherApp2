16c98f547858ff2eb013fd1f3297e96982280ca5
Ovsyannikov_M <maksim.ov@mail.ru>
2022-09-15 23:37:16 +0300

    delete comments

--- app/src/main/java/com/example/weatherapp2/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/weatherapp2/ui/home/HomeViewModel.kt
@@ -11,8 +11,6 @@ import kotlinx.coroutines.launch
 
 class HomeViewModel(
     private val repository: Repository
-    //private val cityWeatherRepoImpl: CityWeatherRepoImpl,
-    //private val localRepo: LocalRepo
 ) : ViewModel() {
 
     var cityWeatherList: MutableLiveData<List<CityFullInfo>> = repository.cityWeatherList
@@ -35,93 +33,4 @@ class HomeViewModel(
            repository.getAllCitiesInfoFromRepo()
         }
     }
-/*
-    private suspend fun loadOneCityInfoFromApi(cityFullInfo: CityFullInfo, language: String): CityFullInfo {
-        return cityWeatherRepoImpl.getCityWeatherFullInfo(cityFullInfo, language)
-    }
-
-    private suspend fun getCitiesCoordinatesList() = withContext(job + Dispatchers.IO) {
-        Log.d("getCitiesCoordinatesList", "here")
-        try {
-            launch {
-                val temp = localRepo.getAllCityFullInfo()
-                withContext(Dispatchers.Main) {
-                    cityFullInfo.clear()
-                    cityFullInfo.addAll(temp)
-                }
-            }.join()
-            true
-        } catch (e: Throwable) {
-            Log.d("HomeViewModel.kt: getCitiesCoordinatesList()", e.toString())
-            false
-        }
-    }
-
-    private suspend fun getCitiesInfoFromApi(language: String) = withContext(job + Dispatchers.IO) {
-        try {
-            launch {
-                resultForAllCitiesFromApi.clear()
-                if (LocalDataCache.getInternetAccess()) {
-                    cityFullInfo.forEach {
-                        resultForAllCitiesFromApi.add(
-                            loadOneCityInfoFromApi(it, language)
-                        )
-                    }
-                    withContext(Dispatchers.Main) {
-                        cityWeatherList.postValue(resultForAllCitiesFromApi)
-                    }
-                }
-            }.join()
-            true
-        } catch (e: Throwable) {
-            Log.d("HomeViewModel.kt: getCitiesInfoFromApi()", e.toString())
-            false
-        }
-    }
-
-    private suspend fun getAllCitiesInfoFromRepo() = withContext(job + Dispatchers.IO) {
-        Log.d("getAllCitiesInfoFromRepo", "here")
-        try {
-            launch {
-                resultForAllCitiesFromRepo.clear()
-                for (cityFullInfo in localRepo.getAllCityFullInfo()) {
-                    if (cityFullInfo.current != null) {
-                        resultForAllCitiesFromRepo.add(cityFullInfo)
-                    }
-                }
-                if (resultForAllCitiesFromRepo.isNotEmpty()) {
-                    Log.d("TAG", resultForAllCitiesFromRepo.toString())
-                    withContext(Dispatchers.Main) {
-                        cityWeatherList.postValue(resultForAllCitiesFromRepo)
-                    }
-                }
-            }.join()
-            true
-        } catch (e: Throwable) {
-            Log.d("HomeViewModel.kt: getCitiesInfoFromRepo()", e.toString())
-            false
-        }
-    }
-
-    private suspend fun loadCitiesToRepo(citiesFullInfo: List<CityFullInfo>) = withContext(
-        job + Dispatchers.IO
-    ) {
-        Log.d("loadCitiesToRepo", "here")
-        try {
-            launch {
-                for (cityFullInfo in citiesFullInfo) {
-                    if (localRepo.getOneCityFullInfo(cityFullInfo.id!!) != null) {
-                        localRepo.updateCityFullInfo(cityFullInfo)
-                    } else {
-                        localRepo.insertCityFullInfo(cityFullInfo)
-                    }
-                }
-            }
-            true
-        } catch (e: Throwable) {
-            Log.d("HomeViewModel.kt: loadCitiesToRepo()", e.toString())
-            false
-        }
-    }
-     */
 }
